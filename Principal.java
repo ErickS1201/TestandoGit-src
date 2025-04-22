fb676e5599f509290cbbef7508e3949634803cdb
ErickS1201 <ericksantana25800@gmail.com>
2025-04-22 15:19:08 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -13,6 +13,7 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        private int num = 12;
         System.out.println("Hello World");
         System.out.println("Faca");
         System.out.println("Imprimindo mais uma linha");
@@ -21,6 +22,7 @@ public class Principal {
         System.out.println("Tentando gerar conflito");
         System.out.println("adianta po");
         System.out.println("quero cafe com nescau");
+        System.out.println(num);
         
               
     }
